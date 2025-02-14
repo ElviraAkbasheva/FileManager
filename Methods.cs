ef756e190050683b757260bd1e13b29599d28703
ElviraAkbasheva <165509920+ElviraAkbasheva@users.noreply.github.com>
2025-02-15 01:13:30 +0300

    First upload

---
+++ Methods.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace FileManager
+{
+    public static class Methods
+    {
+        public delegate void Action(FileInfo file, DateTime time);
+        public static void CreateFiles(string dirPath, int numOfFile)
+        {
+            for (int i = 1; i <= numOfFile; i++)
+            {
+                using (File.Create(Path.Combine(dirPath, $"File{i}.txt"))) { }
+            }
+        }
+        public static void ActionsWithFiles(string dirPath, Action action, DateTime time = default)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(dirPath);
+            if (dirInfo.Exists)
+            {
+                FileInfo[] files = dirInfo.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    try
+                    {
+                        action(file, time);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        Console.WriteLine($"Нет прав на доступ к файлу {file.FullName}.");
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine($"Ошибка при работе с файлом {file.FullName}: {ex.Message}");
+                    }
+                }
+            }
+        }
+        public static void PrintInfo(FileInfo file, DateTime time = default)
+        {
+            using (StreamReader sr = new StreamReader(file.FullName))
+            {
+                string lineForPrint = $"{file.Name}:";
+                string tempLine;
+                while ((tempLine = sr.ReadLine()) != null)
+                {
+                    lineForPrint += $" {tempLine}";
+                }
+                Console.WriteLine(lineForPrint);
+            }
+        }
+
+        public static void WriteToFile(FileInfo file, DateTime time)
+        {
+            using (StreamWriter sw = new StreamWriter(file.FullName, true, Encoding.UTF8))
+            {
+                if (time == default)
+                {
+                    sw.WriteLine(file.Name);
+                }
+                else
+                {
+                    sw.WriteLine(time);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using static FileManager.Methods;
+
+namespace FileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirPath_1 = @"c:\Otus\TestDir1";
+            string dirPath_2 = @"c:\Otus\TestDir2";
+
+            DirectoryInfo testDir_1 = new DirectoryInfo(dirPath_1);
+            DirectoryInfo testDir_2 = new DirectoryInfo(dirPath_2);
+
+            if (!testDir_1.Exists)
+                testDir_1.Create();
+
+            if (!testDir_2.Exists)
+                testDir_2.Create();
+
+            CreateFiles(dirPath_1, 10);
+            CreateFiles(dirPath_2, 10);
+
+            ActionsWithFiles(dirPath_1, WriteToFile);
+            ActionsWithFiles(dirPath_2, WriteToFile);
+
+            ActionsWithFiles(dirPath_1, WriteToFile, DateTime.Now);
+            ActionsWithFiles(dirPath_2, WriteToFile, DateTime.Now);
+
+            ActionsWithFiles(dirPath_1, PrintInfo);
+            ActionsWithFiles(dirPath_2, PrintInfo);
+        }
+    }
+}
